d9a8fccd4a748739dbbcba8bd1bad62863e23996
hans.abdillh05@gmail.com <hans.abdillh05@gmail.com>
2022-07-15 18:24:03 +0800

    Setup

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Tailwind CDN -->
+  <script src="https://cdn.tailwindcss.com"></script>
+
+  <!-- app.css --> 
+  <link rel="stylesheet" href="./src/app.css"/>
+
+  <!-- Font Awesome 6 -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css" integrity="sha384-/frq1SRXYH/bSyou/HUp/hib7RVN1TawQYja658FEOodR/FQBKVqT9Ol+Oz3Olq5" crossorigin="anonymous"/>  
+
+  <!-- Poppins Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap" rel="stylesheet">
+
+  <!-- Main Title -->
+  <title>Project Monitoring</title>
+</head>
+<body>
+  <h1 class="text-2xl font-medium">
+    Hello world! <i class="fa-solid fa-users"></i>
+  </h1>
+</body>
+</html>
